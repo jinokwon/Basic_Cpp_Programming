1a4c54f1efb3b494e17a99453fd3ba602b66874b
jinokwon <55212787+jinokwon@users.noreply.github.com>
2020-09-29 20:26:23 -0400

    Add files via upload

---
+++ 20_08_31.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Demo
+{
+public:
+  Demo();
+  ~Demo();
+};
+
+Demo::Demo()
+{
+  cout << "Welcome to the constructor!\n";
+}
+Demo::~Demo()
+{
+  cout << "The destructor is now running.\n";
+}
+
+int main()
+{
+  Demo demoObject;
+  cout << "This program demonstrates an object\n";
+  cout << "with a constructor and destroctur.\n";
+  return 0;
+}
